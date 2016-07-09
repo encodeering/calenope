59898d4599e7dd49883da9d339443776fcaf1113
Michael Clausen <clausen@synyx.de>
2016-07-09 21:51:09 +0200

    extracts credential generation from google api
    * api shouldn't know how anything about the generation process, which might differ on android for instance
    
    NOTE
    
    * api can now be seen as a factory for specific google services
    * api credential types usually extend the http request initializer

--- src/test/kotlin/de/synyx/google/calendar/sample/BoardSample.kt
+++ src/test/kotlin/de/synyx/google/calendar/sample/BoardSample.kt
@@ -1,14 +1,20 @@
 package de.synyx.google.calendar.sample
 
+import com.google.api.client.auth.oauth2.Credential
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow
 import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport
-import com.google.api.services.admin.directory.DirectoryScopes
-import com.google.api.services.calendar.CalendarScopes
+import com.google.api.client.util.store.FileDataStoreFactory
+import com.google.api.services.admin.directory.DirectoryScopes.ADMIN_DIRECTORY_RESOURCE_CALENDAR_READONLY
+import com.google.api.services.calendar.CalendarScopes.CALENDAR_READONLY
 import de.synyx.google.calendar.api.service.Board
 import de.synyx.google.calendar.internal.GoogleApi
 import de.synyx.google.calendar.internal.service.DefaultBoard
 import org.joda.time.Duration
 import org.joda.time.LocalDateTime
+import java.io.File
 import java.io.InputStreamReader
 import java.nio.file.Files
 import java.nio.file.Paths
@@ -19,6 +25,12 @@ import java.util.Arrays.asList
  * @author clausen - clausen@synyx.de
  */
 object BoardSample {
+
+    private val name = "calendar"
+
+    private val transport by lazy { GoogleNetHttpTransport.newTrustedTransport () }
+
+    private val scopes = asList (ADMIN_DIRECTORY_RESOURCE_CALENDAR_READONLY, CALENDAR_READONLY)
 
     @JvmStatic fun main(args : Array<String>) {
         val board = board(args[0])
@@ -32,16 +44,19 @@ object BoardSample {
     private fun board(filename : String) : Board {
         val stream = Files.newInputStream (Paths.get (filename))
 
-        val scopes = asList(
-                DirectoryScopes.ADMIN_DIRECTORY_RESOURCE_CALENDAR_READONLY,
-                CalendarScopes.CALENDAR_READONLY
-        )
-
-        val secret = GoogleClientSecrets.load(GoogleApi.jackson(), InputStreamReader (stream))
-
-        val api = GoogleApi(GoogleNetHttpTransport.newTrustedTransport(), scopes) { secret }
+        val secret = GoogleClientSecrets.load (GoogleApi.jackson (), InputStreamReader (stream))
+        val api = GoogleApi (name, transport, credential (secret))
 
         return DefaultBoard(api) { resource -> "Besprechungsraum" == resource.resourceType }
+    }
+
+    private fun credential (secret : GoogleClientSecrets) : Credential {
+        val flow = GoogleAuthorizationCodeFlow.Builder (transport, GoogleApi.jackson (), secret, scopes)
+                .setDataStoreFactory (FileDataStoreFactory (File (".", name)))
+                .setAccessType ("offline")
+                .build ()
+
+        return AuthorizationCodeInstalledApp (flow, LocalServerReceiver ()).authorize ("user")
     }
 
 }
