691e71ddc2d632a960a8903bcb3e2c3b49abd93f
Michael Clausen <clausen@synyx.de>
2016-07-22 11:18:41 +0200

    adds a permission request for contacts
    * application needs runtime permission to read the lists of available google contacts
    
    NOTE
    
    * https://developer.android.com/training/permissions/requesting.html
    * we should add an ready item to the state (e.g ready=true, ..) for ui settings

--- modules/organizer/src/main/kotlin/de/synyx/calenope/organizer/ui/Main.kt
+++ modules/organizer/src/main/kotlin/de/synyx/calenope/organizer/ui/Main.kt
@@ -1,17 +1,53 @@
 package de.synyx.calenope.organizer.ui
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
+import de.synyx.calenope.organizer.Action
+import de.synyx.calenope.organizer.R
+import de.synyx.calenope.organizer.toast
 
 /**
  * @author clausen - clausen@synyx.de
  */
 class Main : AppCompatActivity () {
 
+    companion object {
+        private val PERMISSION = 1000
+    }
+
     override fun onCreate (savedInstanceState : Bundle?) {
         super.onCreate    (savedInstanceState)
 
         setContentView (MainLayout (this))
+    }
+
+    override fun onResume () {
+        super.onResume ()
+
+        authorize ()
+    }
+
+    private fun authorize () {
+        if (ContextCompat.checkSelfPermission (this, Manifest.permission.GET_ACCOUNTS) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale (this, Manifest.permission.GET_ACCOUNTS)) {
+                toast (R.string.request_runtime_permission)
+            }
+
+            ActivityCompat.requestPermissions (this, arrayOf (Manifest.permission.GET_ACCOUNTS), PERMISSION)
+        }
+    }
+
+    override fun onRequestPermissionsResult (requestcode : Int, permissions : Array<out String>, grants : IntArray) {
+        super.onRequestPermissionsResult    (requestcode,       permissions,                     grants)
+
+        when (requestcode) {
+            PERMISSION -> if (grants.any { it == PackageManager.PERMISSION_GRANTED })
+                Application.store ().dispatch (Action.UpdateOverview ())
+        }
     }
 
 }
