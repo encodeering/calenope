774be8a41346e815d97a3df4f828d91129ed00a0
Michael Clausen <clausen@synyx.de>
2016-08-11 13:29:50 +0200

    replaces overview fragment with a renderable anvil view
    * http://zserge.com/blog/android-mvp-mvvm-redux-history.html
    * https://corner.squareup.com/2014/10/advocating-against-android-fragments.html
    
    NOTE
    
    * rx subjects can be removed - adapter takes care of the event processing now (obsrever)
    ** adapter must be called by the ui thread only as we do not have any additional synchronization mechanism

--- modules/organizer/src/main/kotlin/de/synyx/calenope/organizer/ui/Main.kt
+++ modules/organizer/src/main/kotlin/de/synyx/calenope/organizer/ui/Main.kt
@@ -5,21 +5,42 @@ import android.accounts.AccountManager
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import android.widget.LinearLayout
 import com.google.android.gms.common.AccountPicker
 import com.google.api.client.googleapis.extensions.android.accounts.GoogleAccountManager
 import com.google.api.client.googleapis.extensions.android.gms.auth.UserRecoverableAuthIOException
 import de.synyx.calenope.core.api.service.Board
 import de.synyx.calenope.core.spi.BoardProvider
-import de.synyx.calenope.organizer.R
 import rx.Observable
-import rx.Subscription
+import rx.Observer
 import rx.android.schedulers.AndroidSchedulers
 import rx.schedulers.Schedulers
-import rx.subjects.ReplaySubject
-import rx.subscriptions.CompositeSubscription
+import trikita.anvil.Anvil
+import trikita.anvil.BaseDSL.WRAP
+import trikita.anvil.BaseDSL.centerHorizontal
+import trikita.anvil.BaseDSL.dip
+import trikita.anvil.BaseDSL.margin
+import trikita.anvil.BaseDSL.textSize
+import trikita.anvil.DSL.FILL
+import trikita.anvil.DSL.adapter
+import trikita.anvil.DSL.gridView
+import trikita.anvil.DSL.horizontalSpacing
+import trikita.anvil.DSL.linearLayout
+import trikita.anvil.DSL.numColumns
+import trikita.anvil.DSL.onItemClick
+import trikita.anvil.DSL.orientation
+import trikita.anvil.DSL.relativeLayout
+import trikita.anvil.DSL.sip
+import trikita.anvil.DSL.size
+import trikita.anvil.DSL.text
+import trikita.anvil.DSL.textColor
+import trikita.anvil.DSL.textView
+import trikita.anvil.DSL.verticalSpacing
+import trikita.anvil.RenderableAdapter
 import java.util.*
 import java.util.concurrent.TimeUnit
 import kotlin.properties.Delegates
@@ -27,7 +48,7 @@ import kotlin.properties.Delegates
 /**
  * @author clausen - clausen@synyx.de
  */
-class Main : AppCompatActivity (), Overview.Interaction {
+class Main : AppCompatActivity () {
 
     companion object {
         private val TAG = Main::class.java.name
@@ -38,35 +59,81 @@ class Main : AppCompatActivity (), Overview.Interaction {
         private val PREF_ACCOUNT_NAME = "account-name"
     }
 
-    private val overviewsource = ReplaySubject.create<Collection<String>> (1)
-    private var subscription : Subscription? = null
+    private inner class RxRenderableAdapter<T> (private val view : (value : T, position : Int) -> Unit) : RenderableAdapter (), Observer<Collection<T>> {
+
+        private var last : Collection<T> = emptyList ()
+
+        override fun view (index : Int) {
+            view (getItem (index), index)
+        }
+
+        override fun getItem (position : Int) : T = last.elementAt (position)
+
+        override fun getCount () : Int = last.size
+
+        override fun onNext (t : Collection<T>) {
+            last = t
+            notifyDataSetChanged ()
+        }
+
+        override fun onCompleted () {
+            notifyDataSetChanged ()
+        }
+
+        override fun onError (e : Throwable?) {
+            last = emptyList ()
+            notifyDataSetChanged ()
+        }
+
+    }
+
+    private val tiles by lazy {
+        RxRenderableAdapter<String> { item, position ->
+            relativeLayout {
+                size (WRAP, WRAP)
+                orientation (LinearLayout.VERTICAL)
+
+                textView {
+                    size (WRAP, WRAP)
+                    text (item)
+                    textSize (sip (10.toFloat ()))
+                    textColor (Color.BLACK)
+                    centerHorizontal ()
+                    margin (dip (20))
+                }
+            }
+        }
+    }
 
     private var board : Board? by Delegates.observable (null as Board?) {
         property, previous, board ->
-            oauth { all ().map { it.id () } }.subscribe (overviewsource)
+            oauth { all ().map { it.id () } }.observeOn (AndroidSchedulers.mainThread ()).subscribe (tiles)
     }
 
     override fun onCreate (savedInstanceState : Bundle?) {
         super.onCreate    (savedInstanceState)
 
-        setContentView (R.layout.main)
+        Anvil.mount (findViewById (android.R.id.content)) {
+            linearLayout {
+                size (FILL, FILL)
+                orientation (LinearLayout.VERTICAL)
 
-        subscription = CompositeSubscription (bindOverview (overviewsource))
+                gridView {
+                    size (FILL, FILL)
+                    adapter (tiles)
+                    numColumns (2)
+                    horizontalSpacing (dip (0))
+                    verticalSpacing   (dip (0))
+                    onItemClick { adapter, view, position, id -> onOverviewClick (adapter.getItemAtPosition (position) as String? ?: "") }
+                }
+            }
+        }
     }
 
     override fun onResume () {
         super.onResume ()
 
         update ()
-    }
-
-    override fun onDestroy () {
-        try {
-            super.onDestroy ()
-        } finally {
-            subscription?.unsubscribe ()
-            subscription = null
-        }
     }
 
     override fun onActivityResult (requestcode : Int, resultcode : Int, data : Intent?) {
@@ -90,18 +157,8 @@ class Main : AppCompatActivity (), Overview.Interaction {
         }
     }
 
-    override fun onOverviewClick (name : String) {
+    private fun onOverviewClick (name : String) {
         Log.d (TAG, "Clicked on $name")
-    }
-
-    private fun bindOverview (source : Observable<Collection<String>>) : Subscription? {
-        return source.subscribeOn (Schedulers.io ()).observeOn (AndroidSchedulers.mainThread ()).subscribe ({
-            val overview = fragmentManager.findFragmentById (R.id.overview)
-
-            if (overview is Overview) {
-                overview.update (it)
-            }
-        })
     }
 
     private fun update () = authenticate (success = { board = board (it) })
