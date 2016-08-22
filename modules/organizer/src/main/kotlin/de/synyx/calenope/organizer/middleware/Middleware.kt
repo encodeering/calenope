2abd8e96ecfe8c0def76edfce930d2f3a617ddd9
Michael Clausen <clausen@synyx.de>
2016-08-22 20:26:39 +0200

    adds a middleware abstraction with bounded types
    * default fun can be used to dispatch an action asynchronuously
    
    NOTE
    
    * synchronuous dispatch calls may lead to lock situations; while its not that likely for dispatch calls inside the middleware dispatch method, it can be avoided by 'outer' calls

---
+++ modules/organizer/src/main/kotlin/de/synyx/calenope/organizer/middleware/Middleware.kt
@@ -0,0 +1,22 @@
+package de.synyx.calenope.organizer.middleware
+
+import de.synyx.calenope.organizer.Action
+import de.synyx.calenope.organizer.State
+import de.synyx.calenope.organizer.ui.Application
+import rx.Observable
+import rx.android.schedulers.AndroidSchedulers
+import rx.schedulers.Schedulers
+import trikita.jedux.Store
+import java.util.concurrent.TimeUnit
+
+/**
+ * @author clausen - clausen@synyx.de
+ */
+
+interface Middleware : Store.Middleware<Action<*>, State> {
+
+    fun fire (action : Action<*>) = observable (0).subscribe { Application.store ().dispatch (action) }
+
+    fun observable (delay : Long) = Observable.timer (delay, TimeUnit.MILLISECONDS, Schedulers.io ()).observeOn (AndroidSchedulers.mainThread ())
+
+}
