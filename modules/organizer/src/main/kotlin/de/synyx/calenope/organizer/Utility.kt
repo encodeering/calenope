e2503d3f4d3400dd9d2276c8e25d8a48c48d7163
Michael Clausen <clausen@synyx.de>
2016-08-26 08:24:07 +0200

    adds a context extension to determine the debuggable state
    * http://stackoverflow.com/questions/15696361/differentiating-debug-and-production-build-of-an-android-app

--- modules/organizer/src/main/kotlin/de/synyx/calenope/organizer/Utility.kt
+++ modules/organizer/src/main/kotlin/de/synyx/calenope/organizer/Utility.kt
@@ -1,6 +1,7 @@
 package de.synyx.calenope.organizer
 
 import android.content.Context
+import android.content.pm.ApplicationInfo
 import android.widget.Toast
 
 /**
@@ -14,3 +15,5 @@ fun Context.toast        (message : Int, duration : Int = Toast.LENGTH_SHORT) {
 fun Context.toast        (message : CharSequence, duration : Int = Toast.LENGTH_SHORT) {
     Toast.makeText (this, message,                duration).show ()
 }
+
+fun Context.debuggable () = 0 != applicationInfo.flags and ApplicationInfo.FLAG_DEBUGGABLE
