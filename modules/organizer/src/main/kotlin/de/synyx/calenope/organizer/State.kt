b5a419c18c5114b0db7d7019d4fd686783b04f17
Michael Clausen <clausen@synyx.de>
2016-08-27 12:29:06 +0200

    adds a missing reduction for the calendar selection
    * overview should reference the correct calendar

--- modules/organizer/src/main/kotlin/de/synyx/calenope/organizer/State.kt
+++ modules/organizer/src/main/kotlin/de/synyx/calenope/organizer/State.kt
@@ -41,6 +41,7 @@ interface State {
             when (action) {
                 is Action.Synchronize   ->                                      action.state.overview
                 is Action.UpdateOverview -> previous.overview.copy (calendars = action.calendars)
+                is Action.SelectCalendar -> previous.overview.copy (selection = action.name)
                 else                     -> previous.overview
             }
 
