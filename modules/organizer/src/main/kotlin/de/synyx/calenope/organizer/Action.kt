0899e8ff112d8070783611063aff57c2ce7d60f7
Michael Clausen <clausen@synyx.de>
2016-08-27 12:29:06 +0200

    adds an action to open the weekview activity
    * open the weekview and then stops dispatch

--- modules/organizer/src/main/kotlin/de/synyx/calenope/organizer/Action.kt
+++ modules/organizer/src/main/kotlin/de/synyx/calenope/organizer/Action.kt
@@ -3,6 +3,7 @@ package de.synyx.calenope.organizer
 import android.content.Context
 import de.synyx.calenope.organizer.middleware.FlowMiddleware
 import de.synyx.calenope.organizer.ui.Settings
+import de.synyx.calenope.organizer.ui.Weekview
 
 /**
  * @author clausen - clausen@synyx.de
@@ -18,5 +19,7 @@ interface Action {
     data class Open           (override val context : Context, override val screen : Class<out Context>) : FlowMiddleware.Open, Action
 
     data class OpenSettings   (override val context : Context) : FlowMiddleware.Open by Open (context, Settings::class.java), Action
+
+    data class OpenWeekview   (override val context : Context) : FlowMiddleware.Open by Open (context, Weekview::class.java), Action
 
 }
