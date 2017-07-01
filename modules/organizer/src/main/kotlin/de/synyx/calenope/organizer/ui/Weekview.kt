b50fa0e7c10009d5ac73d101e1b7e25ce335f299
Michael Clausen <clausen@synyx.de>
2017-07-01 16:59:59 +0200

    reconfigures back button on weekview activity
    * back button should return from the editors view as it feels more natural this way. we can than use tha fab for a different action, eg save

--- modules/organizer/src/main/kotlin/de/synyx/calenope/organizer/ui/Weekview.kt
+++ modules/organizer/src/main/kotlin/de/synyx/calenope/organizer/ui/Weekview.kt
@@ -2,6 +2,9 @@ package de.synyx.calenope.organizer.ui
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import de.synyx.calenope.organizer.Application
+import de.synyx.calenope.organizer.Interact
+import de.synyx.calenope.organizer.Interaction
 
 /**
  * @author clausen - clausen@synyx.de
@@ -24,6 +27,15 @@ class Weekview : AppCompatActivity () {
             layout.close ()
         } finally {
             super.onDestroy ()
+        }
+    }
+
+    override fun onBackPressed () {
+        val interaction = Application.store.state.events.interaction
+
+        when (interaction) {
+            is Interaction.Read -> return super.onBackPressed ()
+            else -> Application.dispatch (Interact (Interaction.Read))
         }
     }
 }
